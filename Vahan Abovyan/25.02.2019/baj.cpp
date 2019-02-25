e5406ec5baaeaa3107700a88c4f8248ea4ad6365
vahan024124 <abovayn1992v@gmail.com>
2019-02-25 23:34:08 +0400

    bajanarar

---
+++ Vahan Abovyan/25.02.2019/baj.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1,num2,num3;
+    cout <<"num1="; cin >>num1;
+
+    for ( int i=1; i<=num1/2; i++ )
+        if (num1%i==0) cout <<i<<"\n";
+        cout <<num1<<  "num1";
+
+
+cout <<"\n"<< "num2="; cin >>num2;
+
+    for ( int i=1; i<=num2/2; i++ )
+        if (num2%i==0) cout <<i<<"\n";
+        cout <<"\n" <<num2 << "\num2";
+
+
+
+        cout << "\n"<< "num3="; cin >>num3;
+
+    for ( int i=1; i<=num3/2; i++ )
+        if (num3%i==0) cout <<i<<"\n";
+        cout <<num3<<"\num3";
+
+
+
+return 0;
+}
+~                                                                                                                                                                       
+~           
