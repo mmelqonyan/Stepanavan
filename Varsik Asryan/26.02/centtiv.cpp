ebc9f7e8c1d1d68f0c426be06e8d39c336433907
Asrvars <varsik.asryan75@gmail.com>
2019-02-26 23:51:55 +0400

    kent

---
+++ Varsik Asryan/26.02/centtiv.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+ 
+int main(){
+
+    int tiv;
+    cout<< "tiv";
+    cin>>tiv;
+    if ( tiv%2!=0){
+         cout<< "Duq nermucel kent tiv";
+       }
+       else
+       {
+          cout<<"dzer tivy zuyg e";
+          
+
+       }
+    
+    
+
+    
+}
