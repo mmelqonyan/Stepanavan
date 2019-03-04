362ead68eb5748e2bb68ca2f0b7f68f83192c449
vahan024124 <abovayn1992v@gmail.com>
2019-03-04 23:53:23 +0400

    bash

---
+++ Vahan Abovyan/04.04.2019/bash.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+cd ./$1
+ls -la
+echo "\n enter a file name "
+read file1
+
+        if [ -f $file1 ]
+        then
+        echo "file is exist  $file1\n"
+        else
+        echo"file not exist \n"
+        fi
+~                                                                               
+~                                                                               
+~                                                                               
+~           
