38b45f77468f4c2f8815e8c84571ff498c849156
Arman199116 <arman.ochinyan@gmail.com>
2019-03-18 23:25:01 +0400

    1 figure

--- Arman Ochinyan/18.03/chess.py
+++ Arman Ochinyan/18.03/chess.py
@@ -10,128 +10,58 @@ for i in range(8):
     for j in range(8):
         board[i].append(symbol)
 
-spitakZinvor = u'| \u265F'
-spitakNavak = u'| \u265C'
-spitakDzi = u'| \u265E'
-spitakOficer = u'| \u265D'
 spitakTaguhi = u'| \u265B'
-spitakTagavor = u'| \u265A'
 
-sevZinvor = u'| \u2659'
-sevNavak = u'| \u2656'
-sevDzi = u'| \u2658'
-sevOficer = u'| \u2657'
-sevTaguhi = u'| \u2655'
-sevTagavor = u'| \u2654'
-
-wh_chk = [spitakZinvor,spitakNavak,spitakDzi,spitakOficer,spitakTaguhi,spitakTagavor ]
-bl_chk = [sevZinvor,sevNavak,sevDzi,sevOficer,sevTaguhi,sevTagavor ]
-
-for i in range(8):
-	board[1][i] = spitakZinvor 
-	board[6][i] = sevZinvor
-
-	board[0][0] = spitakNavak 
-	board[0][7] = spitakNavak 
-	board[0][1] = spitakDzi 
-	board[0][6] = spitakDzi 
-	board[0][2] = spitakOficer 
-	board[0][5] = spitakOficer 
-	board[0][3] = spitakTaguhi 
-	board[0][4] = spitakTagavor
-
-	board[7][0] = sevNavak
-	board[7][7] = sevNavak 
-	board[7][1] = sevDzi
-	board[7][6] = sevDzi
-	board[7][2] = sevOficer 
-	board[7][5] = sevOficer
-	board[7][3] = sevTaguhi 
-	board[7][4] = sevTagavor 
+board[5][5] = spitakTaguhi 
+	
 
 def Board():
-	c = ['    A ',' B ',' C ',' D ',' E ',' F ',' J ',' H']
+	c = ['    A ',' B ',' C ',' D ',' E ',' F ',' G ',' H']
 	print(*c)
 	for x in range(len(board)):
 		print(x+1, *board[x])
 		print()
 Board()
 
-class game:
-	def __init__(self,fig_color):
+def game_mv():
+	get2 = get1 = set2 = set1 = 0
+
+	get_fig = input("Select figure " ).lower()
 		
-		self.fig_color = fig_color
 
-	def game_mv(self,arr):
+	while len(get_fig) != 2 or not get_fig[1].isdigit() or not get_fig[0].isalpha() or 0 > get1 or get1 > 8 or 1 > get2 or get2 > 8 :
+		get_fig = input("Select correct index figure " ).lower()
+		get1 = ord(get_fig[0])-97
+		get2 = ord(get_fig[1])-48
+                                                             
+	get1 = ord(get_fig[0])-97
+	get2 = ord(get_fig[1])-48
 
-		self.fig_chk = arr
-		
-		
-		get2 = get1 = set2 = set1 = 0
+	while board[get2-1][get1] != spitakTaguhi :
 
-		get_fig = input("Select %s figure " % self.fig_color)
-		get_fig = get_fig.lower()
-
-		while len(get_fig) != 2 or not get_fig[1].isdigit() or not get_fig[0].isalpha():
-			get_fig = input("Select correct index %s figure " % self.fig_color)
-			get_fig = get_fig.lower()
-
+		get_fig = input("Select correct index  figure ").lower()
 		get1 = ord(get_fig[0])-97
 		get2 = ord(get_fig[1])-48
 
-		check = False
-	
-		if board[get2-1][get1] not in self.fig_chk:
-			check = True
+		
+	set_fig = input("Select where do you want to go? ").lower()
+		
+	set1 = ord(set_fig[0])-97
+	set2 = ord(set_fig[1])-48
 
-		while check or 0 > get1 or get1 > 8 or 1 > get2 or get2 > 8 :
-
-			get_fig = input("Select correct index %s figure " % self.fig_color)
-			get_fig = get_fig.lower()
-			get1 = ord(get_fig[0])-97
-			get2 = ord(get_fig[1])-48
-
-			if board[get2-1][get1] in self.fig_chk:
-				check = False
-
-
-		set_fig = input("Select where do you want to go? ")
-		set_fig = set_fig.lower()
-
-		while len(set_fig) != 2 or not set_fig[1].isdigit() or not set_fig[0].isalpha():
-			set_fig = input("Select correct index where do you want to go? ")
-			set_fig = set_fig.lower()
-
+	while len(set_fig) != 2 or not set_fig[1].isdigit() or not set_fig[0].isalpha() or 0 > set1 or set1 > 8 or 1 > set2 or set2 > 8:
+		set_fig = input("Select correct index where do you want to go? ").lower()
 		set1 = ord(set_fig[0])-97
 		set2 = ord(set_fig[1])-48
-		check = False
-		
-		if board[set2-1][set1] in self.fig_chk:
-			check = True
 
-		while check or 0 > set1 or set1 > 8 or 1 > set2 or set2 > 8 or len(set_fig) != 2:
-			
-			set_fig = input("Select correct index where do you want to go? ")
-			set_fig = set_fig.lower()
-			set1 = ord(set_fig[0])-97
-			set2 = ord(set_fig[1])-48
+	board[set2-1][set1] = board[get2-1][get1]
+	board[get2-1][get1] = '|__'
 
-			if board[set2-1][set1] not in self.fig_chk:
-				check = False
-			
-		board[set2-1][set1] = board[get2-1][get1]
-		board[get2-1][get1] = '|__'
-		
-for x in range(1,100):
 
-	if x % 2 == 0:
-		w = game('white')
-		w.game_mv(wh_chk)
-	else:
-		b = game('black')
-		b.game_mv(bl_chk)
+	print("Exemple (A2) or (d5)")	
+	
+	game_mv()
 
 	Board()
 
-
-
+game_mv()
