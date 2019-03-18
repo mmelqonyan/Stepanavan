5e87704373a80807c19cb88b167f67e4b60ef9e4
Arman199116 <arman.ochinyan@gmail.com>
2019-03-18 10:53:32 +0400

    check input string length & add isalpha(),isdiit() func

--- Arman Ochinyan/18.03/chess.py
+++ Arman Ochinyan/18.03/chess.py
@@ -70,6 +70,10 @@ class game:
 		get2 = get1 = set2 = set1 = 0
 
 		get_fig = input("Select %s figure " % self.fig_color)
+
+		while len(get_fig) != 2 or not get_fig[1].isdigit() or not get_fig[0].isalpha():
+			get_fig = input("Select correct index %s figure " % self.fig_color)
+
 		get1 = ord(get_fig[0])-97
 		get2 = ord(get_fig[1])-48
 
@@ -79,7 +83,7 @@ class game:
 		if board[get2-1][get1] not in self.fig_chk:
 			check1 = True
 
-		while check1 or 0 > get1 or get1 > 8 or 1 > get2 or get2 > 8  :
+		while check1 or 0 > get1 or get1 > 8 or 1 > get2 or get2 > 8 :
 
 			get_fig = input("Select correct index %s figure " % self.fig_color)
 			get1 = ord(get_fig[0])-97
@@ -90,13 +94,16 @@ class game:
 
 
 		set_fig = input("Select where do you want to go? ")
+		while len(set_fig) != 2 or not set_fig[1].isdigit() or not set_fig[0].isalpha():
+			set_fig = input("Select correct index where do you want to go? ")
+
 		set1 = ord(set_fig[0])-97
 		set2 = ord(set_fig[1])-48
 
 		if board[set2-1][set1] in self.fig_chk:
 			check = True
 
-		while check or 0 > set1 or set1 > 8 or 1 > set2 or set2 > 8:
+		while check or 0 > set1 or set1 > 8 or 1 > set2 or set2 > 8 or len(set_fig) != 2:
 			
 			set_fig = input("Select correct index where do you want to go? ")
 			set1 = ord(set_fig[0])-97
