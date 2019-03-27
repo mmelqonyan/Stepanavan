fa12337db816032e4aa5ba1f675b43f07706f566
Arman199116 <arman.ochinyan@gmail.com>
2019-03-27 22:41:29 +0400

    parrent person

--- Arman Ochinyan/26.03/person.py
+++ Arman Ochinyan/26.03/person.py
@@ -6,34 +6,55 @@ import os
 
 class person:
     
-	kjanq = 10
+	def __init__(self,zenq,tariq):
+		self.zenq = zenq
+		self.kjanq = tariq
 
 	def Krakel(self):
-		
-		return 'Արագությունը 100,Մնաց - %d փամփուշտ' %  (100-self.zenq)
+		return 'արագությունը 100,Ունեք՝ %d կյանք,Մնաց` - %d փամփուշտ' %  (self.kjanq,100-self.zenq)
 
 	def Spanel(self):
 		aragutyun = 100
+		
 		if (self.kjanq - (self.zenq // 10)) <= 0:
-			return 'Արագությունը 0,Դուք Զոհվեցիք'
+			return 'արագությունը 0,Դուք Զոհվեցիք'
 		
 		aragutyun = aragutyun-((self.zenq // 10)*10)
-		return 'Արագությունը %d, մնաց %d Կյանք' % (aragutyun,self.kjanq - (self.zenq // 10) )
+		return 'արագությունը %d,Մնաց` %d Կյանք' % (aragutyun,self.kjanq - (self.zenq // 10) )
+
+class child(person):
+
+	def __init__(self,anun,zenq,tariq):
+		person. __init__(self,zenq,tariq)
+		self.anun = anun
+
+	def Spanel(self):
+		return self.anun+' ձեր ' + person.Spanel(self)
+
+	def Krakel(self):
+		return self.anun+' ձեր ' + person.Krakel(self)
+
 print("Կյանքը 10 Փամփուշտների առավելագույն թիվ 100  ")
+anun = input("Մուտքագրեք ձեր անունը ")
+tariq = int(input("Մուտքագրեք ձեր tariqy "))
+if tariq < 18:
+	print("Դուք չափահաս չեք հաջողություն")
+	sys.exit()
 check = int(input("Կրակել(1),թե պաշտպանվել(2) "))
 
 if check == 1:
-	krakel = person()
+	
 	patron = int(input("Որքան անգամ կրակել "))
 	while patron > 100:
-		patron = int(input("Մուտքագրեք 100 ից փոքր թիվ"))
-	krakel.zenq = patron
+		patron = int(input("Մուտքագրեք 100 ից փոքր թիվ "))
+	krakel = child(anun,patron,tariq)
+
 	print(krakel.Krakel())
 elif check == 2:
 
-	spanel = person()
 	patron = int(input("Որքան անգամ կրակել "))
-	spanel.zenq = patron
+	spanel = child(anun,patron,tariq)
+
 	print(spanel.Spanel())
 else:
 	print("Կրկնեք նորից")
