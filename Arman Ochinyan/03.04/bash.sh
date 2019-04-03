ffa72901189a719eca9cc31c6d5aed769e395706
Arman199116 <47755796+Arman199116@users.noreply.github.com>
2019-04-03 22:48:46 +0400

    Delete bash.sh

--- Arman Ochinyan/03.04/bash.sh
+++
@@ -1,7 +0,0 @@
-# օգտագորխեք ՛echo 'alias your_name="bash filename.sh"' >> ~/.bashrc՛ հրամանը որպեսզի անունը գրելով տարբեր տերմինալներում հրամանը աշխատի
-#!/bin/bash
-alias your_name='bash bash.sh'
-
-cd $1
-ls -la
-
