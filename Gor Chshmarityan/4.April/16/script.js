e8a4721775af63e23d0bf65173fe9e2c68ca8964
Gor1194 <gorchshmarityan94@gmail.com>
2019-04-17 01:22:43 +0400

    Update table

--- Gor Chshmarityan/4.April/16/script.js
+++ Gor Chshmarityan/4.April/16/script.js
@@ -1,31 +1,41 @@
-let row = document.getElementById("row").value;
-let col = document.getElementById("col").value;
 
 function table() {
+    let row = document.getElementById("row").value;
+    let col = document.getElementById("col").value;
     let body = document.getElementsByTagName('body')[0];
     let table = document.createElement("TABLE");
-
-    for (let i = 0; i < row; i++) {
-        let tr = document.createElement("TR");
-        for (let j = 0; j < col; j++) {
-            let td = document.createElement("TD");
-            td.appendChild(document.createTextNode(""))
-            tr.appendChild(td)
+    if (checkUserInput(row) && checkUserInput(col)) {
+        var temp = document.querySelectorAll("table");
+        if (temp.length > 0) {
+            temp[0].remove();
         }
-        table.appendChild(tr);
+        document.getElementById("span").innerHTML = ""
+
+        for (let i = 0; i < row; i++) {
+            let tr = document.createElement("TR");
+            for (let j = 0; j < col; j++) {
+                let td = document.createElement("TD");
+                td.appendChild(document.createTextNode(""))
+                tr.appendChild(td)
+            }
+            table.appendChild(tr);
+        }
+        body.appendChild(table)
     }
-    body.appendChild(table)
+    else {
+        var temp = document.querySelectorAll("table");
+        if (temp.length > 0) {
+            temp[0].remove();
+        }
+        document.getElementById("span").innerHTML = "Row must be 1-10, and columns 1-10 , enter correct data."
+
+    }
+    document.getElementById("row").value = ""
+    document.getElementById("col").value = ""
 }
+
 
 function checkUserInput(userInput) {
     return !isNaN(userInput) && userInput > 0 && userInput < 11
 }
 
-function start() {
-    if (checkUserInput(row) && checkUserInput(col)){
-        table()
-    }
-    else {
-        alert("Row must be 1-10, and columns 1-10 \n\nReload the page and enter CORRECT data.")
-    }
-}
