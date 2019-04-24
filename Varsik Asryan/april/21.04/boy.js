4e60a4da6e163f844de2d5ef04caf38f2e50ed63
Asrvars <varsik.asryan75@gmail.com>
2019-04-24 10:30:40 +0400

    news

---
+++ Varsik Asryan/april/21.04/boy.js
@@ -0,0 +1,27 @@
+ 
+
+
+
+
+    var table = document.createElement("table")
+    table.setAttribute("background-color: #c7b39b;")
+    var span = document.createElement("span")
+    for (vari = 0; i < 10; i++) {
+        var tr = document.createElement("tr")
+         tr.setAttribute("border color:red")
+        for (var j = 0; j < 10; j++) {
+
+            var td = document.createElement("td")
+            var button = document.createElement("button")
+            button.setAttribute("style", "height: 45px; width:45px","border color:red")
+            button.setAttribute("onclick")
+           tr.appendChild(td)
+        td.appendChild(button)
+
+            
+        }
+        table.appendChild(tr)
+    }
+    body.appendChild(table)
+    
+  
