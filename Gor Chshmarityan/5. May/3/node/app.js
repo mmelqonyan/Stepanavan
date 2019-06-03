be9c0a1c94f4d1fb5a7879f06f858590038f4c0f
Gor <gorchshmarityan94@gmail.com>
2019-06-03 23:38:55 +0400

    node

---
+++ Gor Chshmarityan/5. May/3/node/app.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var mongodb = require('mongodb');
+
+var dbConn = mongodb.MongoClient.connect('mongodb://localhost:27017/gor' );
+
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.post('/post-feedback', function (req, res) {
+    dbConn.then(function(db) {
+        delete req.body._id; // for safety reasons
+        db.collection('feedbacks').insertOne(req.body);
+    });    
+    res.send('Hello '+ req.body.name+'<br>'+ 'Your email is: ' + req.body.email + '<br>' + 'Your last message was: ' + req.body.comment);
+
+});
+
+app.get('/view-feedbacks',  function(req, res) {
+    dbConn.then(function(db) {
+        db.collection('feedbacks').find({}).toArray().then(function(feedbacks) {
+            res.status(200).json(feedbacks);
+        });
+    });
+});
+
+app.listen(process.env.PORT || 3000, process.env.IP || '0.0.0.0' );
