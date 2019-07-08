1c3e0ba068e9b956ab72fd214f9ad1ae8a5c2502
Arman199116 <arman.ochinyan@gmail.com>
2019-07-09 01:04:54 +0400

    react Aliexpress Buy counter

--- Arman Ochinyan/08.07/aliexpress/hello-world/src/imgs.js
+++ Arman Ochinyan/08.07/aliexpress/hello-world/src/imgs.js
@@ -11,15 +11,15 @@ class Imgs extends React.Component {
 
 	    return (
 	        <div>
-		        <div className="card col-md" style={{width:300+'px'}}>
-	                    <img src={this.props.src}  alt="Responsive image" />   
-	                    <div className="card-body">
-	                        <h4 className="card-title">Գոտի</h4>
-	                        <p className="card-text">Բնական կաշի</p>
-	                        <p className="card-text">Գինը   16000 դր․</p>
-	                        <a className="btn btn-primary" onClick={this.countAdd} >BUY</a>
-	                    	<p>{this.state.counter}</p>
-	                    </div>
+		        <div class="card col-md div" style={{width:320+'px'}}>
+	                <img src={this.props.src}  alt="Responsive image" />   
+	                <div class="card-body">
+	                    <h4 class="card-title">Գոտի</h4>
+	                    <p class="card-text">Բնական կաշի</p>
+	                    <p class="card-text">Գինը   16000 դր․</p>
+	                    <a class="btn btn-primary" onClick={this.countAdd} >BUY</a>
+	                    <p>{this.state.counter}</p>
+	                </div>
 	            </div>
 	        </div>
 	    );
--- Arman Ochinyan/08.07/aliexpress/hello-world/src/index.js
+++ Arman Ochinyan/08.07/aliexpress/hello-world/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Imgs from './App';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 
-ReactDOM.render(<Imgs  />, document.getElementById('root'));
+ReactDOM.render(<App  />, document.getElementById('root'));
 
 
 
