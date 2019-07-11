7400d7c856bddc346dc05487ccde610b52251df1
Arman199116 <arman.ochinyan@gmail.com>
2019-07-11 22:53:51 +0400

    aliexpress & show input values

--- Arman Ochinyan/11.07/forminput/src/App.js
+++ Arman Ochinyan/11.07/forminput/src/App.js
@@ -1,37 +1,48 @@
 
 import React from 'react';
-import ReactDOM from "react-dom";
 
 class App extends React.Component{
 	 constructor(props) {
       super(props);
-      this.state = {login: '', password: '',name : ''};
+      this.state = {login: '', password: '',name : '',show : false};
 
-      
     }
     
     onChangevalue = (event) => {
     	const name = event.target.name;
       	this.setState({[name] : event.target.value});
     }
+    show = () => {
+    	this.setState({show : true});
+    }
+    hide = () => {
+    	
+    	this.setState({login: '', password: '',name : '',show : false});
+    }
     
     render() {
 
-    	let { name, login, password } = {...this.state};
+    	let { name, login, password, show } = {...this.state};
+    	
     	
 	    return (
+
 	    	
 	        <form>
 		        <p><label> Name: <input type="text" name="name" value={name}
-			                        onChange={this.onChangevalue} autoComplete="off"/></label></p>
+			                        onChange={this.onChangevalue} autoComplete="off" /></label></p>
 		        <p><label> Login: <input type="text" name="login" value={login}
 		                         onChange={this.onChangevalue} autoComplete="off"/></label></p>
 
 	            <p><label> Password: <input type="password" name="password" value={password}
 	                            onChange={this.onChangevalue} /></label></p>
+	            <p><label> <input type="button"  value="Show values"
+	                            onClick={this.show} /></label></p>  
+	            <p><label> <input type="button"  value="Hide"
+	                            onClick={this.hide} /></label></p>               
 	
 	          	<br />
-    			<Showvalues name={name} login={login} password={password} />
+    			{show && <Showvalues name={name} login={login} password={password} />}
 	        </form>
 	    );
     }
