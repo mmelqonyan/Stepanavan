14a63a450023419e2fc65140f0a5554d8e64304f
gayane <ggaynane@gmail.com>
2019-07-13 22:40:54 +0400

    input text

--- Gayane Gevorgyan/bloks/src/index.js
+++ Gayane Gevorgyan/bloks/src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-import Bloks from './App';
+import App from './App';
 
-ReactDOM.render(<Bloks />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 
 
