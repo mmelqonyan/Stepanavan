c982eb7256563baa778d957b14082a3d53e817a7
Asrvars <varsik.asryan75@gmail.com>
2019-07-26 12:07:09 +0400

    aysor

--- Varsik Asryan/registr/submit/src/App.js
+++ Varsik Asryan/registr/submit/src/App.js
@@ -12,21 +12,79 @@ class App extends React.Component {
          surname:"",
           email:"",
        
-          password:""
+          password:"",
+          showLog:false,
+          showReg:false,
+          vxod:false,
+          registr:false,
+
         
       };
-      this.vxod = this.vxod.bind(this);
-       this.registr = this.registr.bind(this);
       
 
    }
+
+   showLogin(){
+     if(this.state.showLog === false){
+       this.setState({showLog: true})
+     }
+     else{
+      this.setState({showLog: false});
+     }
+   }
+
+   showRegister() {
+    if(this.state.showReg === true){
+      this.setState({showReg: false});
+ 
+    }
+    else{
+      this.setState({showReg: true});
+ 
+    }
+  }
+
+
+vxod=()=> {  
+   let inputemail = document.getElementById('email').value;
+  
+  
+   let inputpassword = document.getElementById('password').value;
+   if((inputemail=== this.state.email) &&  (inputpassword=== this.state.password)){
+     alert("velcome our servise")
+   }
+   else{
+     alert("please, try again")
+
+   }
+ } 
+
+  registr =()=> {
+   let inputemail = document.getElementById('email').value;
+  
+  
+  
+   if((inputemail=== this.state.email)){
+     alert("aydpisi email arden ka");
+   }
+  
+
+ } 
+
+ 
+
+
 
 
   render() {
     return (
       <form   action="http://localhost:3001/"  method="POST">
       <div   id = "App">
-   
+
+      <input type="button" value="Login" onClick={()=>this.showLogin()}/>
+
+<input type="button" class="btnSubmit"value = "Registration" onClick={()=>this.showRegister()}/>
+      
   
   <div class="text-center">
     <h1>Registration page</h1>
@@ -40,7 +98,8 @@ class App extends React.Component {
       
   
          
-      
+      {
+        this.state.showLog?
         <div class="float-right">
      <p>Email</p>
         <input  type="text" name="email "   placeholder=" Email"  id = "email" />
@@ -48,13 +107,14 @@ class App extends React.Component {
     
         <input type="text" name="Password " placeholder=" Password" id = "password" />
         <div class="form-group">
-                            <input type="submit" class="btnSubmit" value="Sign in" OnClock />
+                            <input type="button" class="btnSubmit" value="Login" onClick={()=>this.vxod()}/>  
                         </div>
                        
        
         
         
         </div>
+        :null}
         
           
           
@@ -62,11 +122,12 @@ class App extends React.Component {
          
        
     
+{
+this.state.showReg? 
+<div>      
+ <p>Name</p>
 
-         
-          <p>Name</p>
-
-<input type="text" name="name "  placeholder=" Name" />
+<input type="text" name= "name"  placeholder="Name" />
 
 <p>Surname</p>
 
@@ -79,14 +140,17 @@ class App extends React.Component {
 
 <p>Password</p>
 
-<input type="text" name="password "  placeholder=" "Password id = "password"   />
+<input type="text" name="password "  placeholder= "Password"id = "password"   />
+
+
 <div class="form-group">
-                            <input type="submit" class="btnSubmit" value="Registration" />
-                        </div>
 
+ 
 
-
-      
+ <input type="button" class="btnSubmit"value = "Registration" onClick={()=>this.registr()}/>
+  </div>
+ </div>:null
+}
 </div>
 
         </form>
@@ -99,31 +163,7 @@ class App extends React.Component {
 }
 
 
- vxod=()=> {  
-  let inputemail = document.getElementById('email').value;
   
-  
-  let inputpassword = document.getElementById('password').value;
-  if((inputemail=== this.state.email) &&  (inputpassword=== this.state.password)){
-    console.log("welcome our service")
-  }
-  else{
-    console.log("Error")
-
-  }
-} 
-
- registr =()=> {
-  let inputemail = document.getElementById('email').value;
-  
-  
-  
-  if((inputemail=== this.state.email)){
-    console.log("aydpisi email arden ka");
-  }
-  
-
-} 
 
 
 export default App ;
