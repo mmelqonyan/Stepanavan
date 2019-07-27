923d0e895ea93f460fa709f8b6f8a15cccb2de89
Asrvars <varsik.asryan75@gmail.com>
2019-07-27 17:31:40 +0400

    re.proekt

--- Varsik Asryan/registr/submit/src/App.js
+++ Varsik Asryan/registr/submit/src/App.js
@@ -1,169 +1,152 @@
 import React from 'react';
-
 import './App.css';
-
+import axios from 'axios';
+const query = require('querystring');
 
 class App extends React.Component {
-  
-  constructor(props){
-        super(props);
-       this.state = {
-         name:"",
-         surname:"",
-          email:"",
-       
-          password:"",
-          showLog:false,
-          showReg:false,
-          vxod:false,
-          registr:false,
 
-        
-      };
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+        name: "",
+        surname: "",
+        email: "",
+        password: ""
+      
+    ,
+
+      showLog: false,
+      showReg: false,
       
 
-   }
+    };
 
-   showLogin(){
-     if(this.state.showLog === false){
-       this.setState({showLog: true})
-     }
-     else{
-      this.setState({showLog: false});
-     }
-   }
 
-   showRegister() {
-    if(this.state.showReg === true){
-      this.setState({showReg: false});
- 
+  }
+
+  showLogin() {
+    if (this.state.showLog === false) {
+      this.setState({ showLog: true })
     }
-    else{
-      this.setState({showReg: true});
- 
+    else {
+      this.setState({ showLog: false });
     }
   }
 
+  showRegister() {
+    if (this.state.showReg === true) {
+      this.setState({ showReg: false });
 
-vxod=()=> {  
-   let inputemail = document.getElementById('email').value;
-  
-  
-   let inputpassword = document.getElementById('password').value;
-   if((inputemail=== this.state.email) &&  (inputpassword=== this.state.password)){
-     alert("velcome our servise")
-   }
-   else{
-     alert("please, try again")
+    }
+    else {
+      this.setState({ showReg: true });
 
-   }
- } 
+    }
+  }
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  }
 
-  registr =()=> {
-   let inputemail = document.getElementById('email').value;
-  
-  
-  
-   if((inputemail=== this.state.email)){
-     alert("aydpisi email arden ka");
-   }
-  
+  handleSubmit = event => {
+    event.preventDefault();
+    let url = 'http://localhost:3001/';
+    const data = {
+      name: this.state.name,
+      surname: this.state.surname,
+      email: this.state.email,
+      password: this.state.password
+    }
+    axios.post(url, query.stringify(data))
+      .then(res => {
+        this.setState({ output: res.data })
+      })
+  }
 
- } 
 
- 
+
+
+
+
+
+
+
 
 
 
 
   render() {
     return (
-      <form   action="http://localhost:3001/"  method="POST">
-      <div   id = "App">
+      <form action="http://localhost:3001/" method="POST">
+        <div id="App">
 
-      <input type="button" value="Login" onClick={()=>this.showLogin()}/>
+          <input type="button" value="Login" onClick={() => this.showLogin()} />
 
-<input type="button" class="btnSubmit"value = "Registration" onClick={()=>this.showRegister()}/>
-      
-  
-  <div class="text-center">
-    <h1>Registration page</h1>
-    <p>Please fill in this form to create an account</p>
-    
-  </div>
-  
-  
-        
-      
-      
-  
-         
-      {
-        this.state.showLog?
-        <div class="float-right">
-     <p>Email</p>
-        <input  type="text" name="email "   placeholder=" Email"  id = "email" />
-      <p>Password</p>
-    
-        <input type="text" name="Password " placeholder=" Password" id = "password" />
-        <div class="form-group">
-                            <input type="button" class="btnSubmit" value="Login" onClick={()=>this.vxod()}/>  
-                        </div>
-                       
-       
-        
-        
+          <input type="button" className="btnSubmit" value="Registration" onClick={() => this.showRegister()} />
+
+
+          <div className="text-center">
+            <h1>Registration page</h1>
+            <p>Please fill in this form to create an account</p>
+
+          </div>
+
+          {
+            this.state.showLog ?
+              <div className="float-right">
+                <p>Email</p>
+                <input type="text" name="email" placeholder=" Email" />
+                <p>Password</p>
+
+                <input type="text" name="password" placeholder=" Password" />
+                <div className="form-group">
+                  <input type="button" className="btnSubmit" value="Login" onClick={this.handleSubmit} />
+                </div>
+
+              </div>
+              : null}
+          {
+            this.state.showReg ?
+              <div>
+                <p>Name</p>
+
+                <input type="text" name="name" placeholder="Name" onChange={this.handleChange} />
+
+                <p>Surname</p>
+
+                <input type="text" name="surname" placeholder=" Surname" onChange={this.handleChange} />
+
+                <p>Email</p>
+
+                <input type="text" name="email" placeholder=" Email" onChange={this.handleChange} />
+
+
+                <p>Password</p>
+
+                <input type="text" name="password" placeholder="Password" onChange={this.handleChange} />
+
+
+                <div className="form-group">
+
+
+
+                  <input type="button" className="btnSubmit" value="Registration" onClick={this.handleSubmit} />
+                </div>
+              </div> : null
+          }
         </div>
-        :null}
-        
-          
-          
 
-         
-       
-    
-{
-this.state.showReg? 
-<div>      
- <p>Name</p>
-
-<input type="text" name= "name"  placeholder="Name" />
-
-<p>Surname</p>
-
-<input type="text" name="surname "  placeholder=" Surname" />
-
-<p>Email</p>
-
-<input type="text" name="surname "  placeholder=" Email" id = "email" />
+      </form>
 
 
-<p>Password</p>
-
-<input type="text" name="password "  placeholder= "Password"id = "password"   />
 
 
-<div class="form-group">
-
- 
-
- <input type="button" class="btnSubmit"value = "Registration" onClick={()=>this.registr()}/>
-  </div>
- </div>:null
-}
-</div>
-
-        </form>
-
-       
-
-    
     );
   }
 }
 
 
-  
 
 
-export default App ;
+
+export default App;
--- Varsik Asryan/registr/submit/src/server.js
+++ Varsik Asryan/registr/submit/src/server.js
@@ -2,21 +2,74 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const MongoClient = require("mongodb").MongoClient;
 const mongo = new MongoClient("mongodb://localhost:27017/", { useNewUrlParser: true });
-
-const app = express()
+const cors = require('cors');
+const app = express();
 app.use(bodyParser.urlencoded({extended:true}))
-app.post('/ ',function(req,res){
-   mongo.connect(function(err, client){
+app.use(cors());
 
-       const db = client.db("varsik");27017
-       const collection = db.collection("data");
-       collection.insertOne({aa:req.body}, function(err, result){
+module.exports = {
+	signup: function(name, surname,email, password){
+		MongoClient.connect(url, function(err, db) {
+		  	db.collection('user').insertOne( {
+				"name": name,
+                                 "surname":surname,
+				"email": email,
+				"password": password
+			},function(err, result){
+				assert.equal(err, null);
+		    	console.log("Saved the user sign up details.");
+			});
+		});
+	},
+	validateSignIn: function(username, password,callback){
+		MongoClient.connect(url, function(err, db){
+			console.log(username,password);
+			db.collection('user').findOne( { email : username ,password: password 
+			},function(err, result){
+				if(result==null){
+					console.log('returning false')
+					callback(false)
+				}
+				else{
+					console.log('returning true')
+					callback(true)
+				}
+			});
+		});
+	}
 
-           console.log(req.body)
-       });
-   });
+}
+
+app.post('/signin', function (req, res) {
+  var user_name=req.body.email;
+  var password=req.body.password;
+  user.validateSignIn(user_name,password,function(result){
+    if(result){
+      res.send('Success')
+    }
+    else{
+      res.send('Wrong username pass')
+    }
+  });
+  
+  
+  
 })
- 
+
+app.post('/signup', function (req, res) {
+  var name=req.body.name;
+  var email=req.body.email;
+  var password=req.body.password;
+
+  if(name && email && password){
+  	user.signup(name, email, password)
+  }
+  else{
+  	res.send('Failure');
+  }
+})
+
+
 
 app.listen(3001, function () {
    console.log('Example app listening on port 3000!');
